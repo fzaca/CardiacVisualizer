c239b93f640be373ee4bd3fd841a9af6354d5cc2
Xukay101 <zaca03zaca@gmail.com>
2023-11-29 22:20:47 -0300

    implement  operation

--- cardiac-core/src/lib.rs
+++ cardiac-core/src/lib.rs
@@ -2,21 +2,23 @@ use std::collections::HashMap;
 
 /// Emulator of the **CARDIAC** cardboard computer.  
 /// info in [wikipedia](wikipedia.org/wiki/CARDboard_Illustrative_Aid_to_Computation)
+/// 
+/// Simulator for tests: [simulator](https://www.cs.drexel.edu/~bls96/museum/cardsim.html)
 pub struct Assembler {
-    memory: [u32; 100],
+    memory: [i32; 100],
     accumulator: i32,
     target: u32,
     flag: bool,
     step: i32,
     input_deck: Vec<i32>,
     output_deck: Vec<i32>,
-    instruction_map: HashMap<u32, fn(&Self, usize)>,
+    instruction_map: HashMap<u32, fn(&mut Self, usize)>,
 }
 
 impl Assembler {
     /// Creates an new `Assembler`
     pub fn new() -> Self {
-        let mut instruction_map: HashMap<u32, fn(&Self, usize)> = HashMap::new();
+        let mut instruction_map: HashMap<u32, fn(&mut Self, usize)> = HashMap::new();
         instruction_map.insert(0, Self::inp);
         instruction_map.insert(0, Self::cla);
         instruction_map.insert(0, Self::add);
@@ -28,7 +30,7 @@ impl Assembler {
         instruction_map.insert(0, Self::jmp);
         instruction_map.insert(0, Self::hrs);
 
-        let mut memory: [u32; 100] = [0; 100];
+        let mut memory: [i32; 100] = [0; 100];
         memory[0] = 1;
         memory[99] = 800;
 
@@ -44,43 +46,55 @@ impl Assembler {
         }
     }
 
-    fn inp(&self, address: usize) {
+    /// Take a number from the input card ("INPut") and place it in the specified memory cell.
+    fn inp(&mut self, address: usize) {
+        if address <= 99 {
+            panic!("Memory address out of range: {}", address);
+        }
+
+        if let Some(value) = self.input_deck.pop() {
+            if !(-999 <= value && value <= 999) {
+                panic!("Value in input card out of range: {}", value);
+            }
+            self.memory[address] = value;
+        } else {
+            self.memory[address] = 0;
+        }
+    }
+
+    fn cla(&mut self, address: usize) {
         println!("inp {address}")
     }
 
-    fn cla(&self, address: usize) {
+    fn add(&mut self, address: usize) {
         println!("inp {address}")
     }
 
-    fn add(&self, address: usize) {
+    fn tac(&mut self, address: usize) {
         println!("inp {address}")
     }
 
-    fn tac(&self, address: usize) {
+    fn sft(&mut self, address: usize) {
         println!("inp {address}")
     }
 
-    fn sft(&self, address: usize) {
+    fn out(&mut self, address: usize) {
         println!("inp {address}")
     }
 
-    fn out(&self, address: usize) {
+    fn sto(&mut self, address: usize) {
         println!("inp {address}")
     }
 
-    fn sto(&self, address: usize) {
+    fn sub(&mut self, address: usize) {
         println!("inp {address}")
     }
 
-    fn sub(&self, address: usize) {
+    fn jmp(&mut self, address: usize) {
         println!("inp {address}")
     }
 
-    fn jmp(&self, address: usize) {
-        println!("inp {address}")
-    }
-
-    fn hrs(&self, address: usize) {
+    fn hrs(&mut self, address: usize) {
         println!("inp {address}")
     }
 
@@ -99,11 +113,11 @@ impl Assembler {
         self.memory[99] = 800;
     }
 
-    pub fn update_cell(&mut self, address: u32, instruction: u32) {
+    pub fn update_cell(&mut self, address: u32, instruction: i32) {
         self.memory[address as usize] = instruction;
     }
 
-    pub fn load_program(&mut self, program: HashMap<u32, u32>) {
+    pub fn load_program(&mut self, program: HashMap<u32, i32>) {
         for (address, instruction) in program {
             self.memory[address as usize] = instruction;
             // println!("Address {address}; Instruction {instruction};")
@@ -111,10 +125,10 @@ impl Assembler {
     }
 
     pub fn next_step(&mut self) {
-        let instruction: u32 = self.memory[self.target as usize];
+        let instruction: i32 = self.memory[self.target as usize];
 
-        let opcode: u32 = instruction / 100;
-        let address: u32 = instruction % 100;
+        let opcode: u32 = (instruction / 100) as u32;
+        let address: u32 = (instruction % 100) as u32;
         
         if let Some(instruction_fn) = self.instruction_map.get(&opcode) {
             instruction_fn(self, address as usize);
